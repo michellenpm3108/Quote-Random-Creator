c3f59d20dc1e973a1b99f8630470a36492323ccb
michellenpm3108 <71855504+michellenpm3108@users.noreply.github.com>
2020-10-02 06:25:24 +0800

    quote random creator
    used math.floor and math.random

---
+++ index.js
@@ -0,0 +1,27 @@
+const quoteDisplay = document.querySelector('#quote')
+const button = document.querySelector('button')
+
+const quotes = [
+    'Do something today that your future self will thankyou for',
+    'Dont worry, be happy!',
+    'Your limitation is only your imagination'
+]
+const colors = ['#f7f0d0', '#e4fcd7', '#d7f6fc','#f0d7fc']
+
+function showQuote() {
+    let randomNumber = Math.random () * quotes.length;
+    let newNumber = Math.floor (randomNumber);
+    let randomQuote = quotes[newNumber];
+    console.log (randomQuote);
+    quoteDisplay.innerHTML = randomQuote 
+}
+function changeColor () {
+    let randomNumber = Math.random() * colors.length;
+    let newNumber = Math.floor (randomNumber);
+    let randomColor = colors[newNumber];
+    console.log (randomColor);
+    const background = document.querySelector ('.frame');
+    background.style.backgroundColor = randomColor;
+}
+button.addEventListener('click', showQuote)
+button.addEventListener('click',changeColor)
